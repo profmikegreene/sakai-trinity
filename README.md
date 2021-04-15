aa246c8121a1be5a41532a490db85d5c193032ee
Michael Greene <profmikegreene@gmail.com>
2021-04-14 20:58:26 -0400

    WIP node and toolbar

--- README.md
+++ README.md
@@ -7,6 +7,25 @@ Mockups for user testing new Sakai UI.
 @fostersdesign  
 @profmikegreene  
 
+## Icons
+
+Icon set used is <https://ionicons.com/>
+
+To add a new icon
+
+1. Select the icon from the list
+2. Click the download SVG button
+3. Save to the icons folder
+4. Add relevant markup
+
+## Docker
+
+```sh
+docker-compose run --rm --service-ports node
+npm install -g browser-sync
+browser-sync start --server --files "./*"
+```
+
 ## TODO
 
 - [ ] figure out how to install browser-sync globally in dockerfile
--- scripts.js
+++ scripts.js
@@ -1,24 +1,17 @@
 const quickSideBar = document.querySelector('.Mrphs-quickSideBar');
 const portalWrapper = document.querySelector('.Mrphs-portalWrapper');
-/*const qsbSites = quickSideBar.querySelector('.Mrphs-qsb-sites');
-const qsbTasks = quickSideBar.querySelector('.Mrphs-qsb-tasks');
-const qsbGrades = quickSideBar.querySelector('.Mrphs-qsb-grades');
-const qsbCalendar = quickSideBar.querySelector('.Mrphs-qsb-calendar');
-const qsbConnections = quickSideBar.querySelector('.Mrphs-qsb-connections');
-
-qsbSites.addEventListener('click',toggleQuickSideBar(event));
-qsbTasks.addEventListener('click',toggleQuickSideBar(event));
-qsbGrades.addEventListener('click',toggleQuickSideBar(event));
-qsbCalendar.addEventListener('click',toggleQuickSideBar(event));
-qsbConnections.addEventListener('click',toggleQuickSideBar(event));
-
-function toggleQuickSideBar(event) {
-	alert('toggle!');
-}*/
 
 quickSideBar.querySelectorAll('.Mrphs-quickIt').forEach(item => {
 	item.addEventListener('click', event => {
 		event.preventDefault();
 		portalWrapper.classList.toggle("quickSideBarExpanded");
+	})
+})
+
+const toolBar = document.querySelector('.Mrphs-toolBar');
+toolBar.querySelectorAll('.Mrphs-sitesNav__menuitem').forEach(item => {
+	item.addEventListener('click', event => {
+		event.preventDefault();
+		item.classList.toggle("is-expanded");
 	})
 })

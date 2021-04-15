216f7aa4cd2b5e9a7e0c59ec6cb92e60ad5d89a6
Shawn <shawn.foster@uwo.ca>
2021-04-14 21:05:34 -0400

    Images and icons missing from my last PR

--- scripts.js
+++ scripts.js
@@ -1,22 +1,7 @@
-const quickSideBar = document.querySelector('.Mrphs-quickSideBar');
-const portalWrapper = document.querySelector('.Mrphs-portalWrapper');
-/*const qsbSites = quickSideBar.querySelector('.Mrphs-qsb-sites');
-const qsbTasks = quickSideBar.querySelector('.Mrphs-qsb-tasks');
-const qsbGrades = quickSideBar.querySelector('.Mrphs-qsb-grades');
-const qsbCalendar = quickSideBar.querySelector('.Mrphs-qsb-calendar');
-const qsbConnections = quickSideBar.querySelector('.Mrphs-qsb-connections');
+const quickSideBar = document.querySelector('.sakai-quickSideBar');
+const portalWrapper = document.querySelector('.sakai-portalWrapper');
 
-qsbSites.addEventListener('click',toggleQuickSideBar(event));
-qsbTasks.addEventListener('click',toggleQuickSideBar(event));
-qsbGrades.addEventListener('click',toggleQuickSideBar(event));
-qsbCalendar.addEventListener('click',toggleQuickSideBar(event));
-qsbConnections.addEventListener('click',toggleQuickSideBar(event));
-
-function toggleQuickSideBar(event) {
-	alert('toggle!');
-}*/
-
-quickSideBar.querySelectorAll('.Mrphs-quickIt').forEach(item => {
+quickSideBar.querySelectorAll('.sakai-quickIt').forEach(item => {
 	item.addEventListener('click', event => {
 		event.preventDefault();
 		portalWrapper.classList.toggle("quickSideBarExpanded");

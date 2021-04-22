4e6c2d59f6e837ebf71a4ffc829be7563e1e71ae
Shawn <shawn.foster@uwo.ca>
2021-04-21 22:18:21 -0400

    Added the table actions bar

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Quick Side Bar
+
 const quickSideBar = document.querySelector('.sakai-quickSideBar');
 const portalWrapper = document.querySelector('.sakai-portalWrapper');
 
@@ -8,6 +10,8 @@ quickSideBar.querySelectorAll('.sakai-quickIt').forEach(item => {
 	})
 })
 
+// Tools Menu/Bar
+
 const toolBar = document.querySelector('.sakai-toolBar');
 toolBar.querySelectorAll('.sakai-sitesNav__menuitem').forEach(item => {
 	item.addEventListener('click', event => {
@@ -15,3 +19,63 @@ toolBar.querySelectorAll('.sakai-sitesNav__menuitem').forEach(item => {
 		item.classList.toggle("is-expanded");
 	})
 })
+
+// Table Actions
+
+const exampleTable = document.querySelector('.sakai-table');
+const tableActions = document.querySelector('.sakai-table-actionsBar');
+const selectedCounter = document.querySelector('.sakai-table-actionsBar-selected > span');
+const selectAllTableItems = exampleTable.querySelector('th.sakai-table-selectItem > input[type="checkbox"]');
+const allTableItems = exampleTable.querySelectorAll('td.sakai-table-selectItem > input[type="checkbox"]');
+const tableActionsClose = tableActions.querySelector('.sakai-table-actionsBar-cancel');
+var selectedItems = new Set(); 
+
+allTableItems.forEach(checkedItem => {
+	checkedItem.addEventListener('change', function() {
+		if (this.checked) {
+			if(selectedItems.size < 1) {
+				tableActions.classList.add("enabled");
+			}
+			selectedItems.add(this);
+			selectedCounter.innerText = selectedItems.size;
+		} else {
+			selectedItems.delete(this);
+			selectedCounter.innerText = selectedItems.size;
+			if(selectedItems.size == 0) {
+				tableActions.classList.remove("enabled");
+			}
+			selectAllTableItems.checked = false;
+		}
+	})
+});
+
+function checkAllTableItems(checkedState) {
+	allTableItems.forEach(tableItem => {
+		tableItem.checked = checkedState;
+		if(checkedState) {
+			// check all boxes:
+			selectedItems.add(tableItem);
+			selectedCounter.innerText = selectedItems.size;
+			tableActions.classList.add("enabled");
+		}
+	});
+	
+	if(!checkedState) {
+		selectedItems = new Set();
+		tableActions.classList.remove("enabled");
+	}
+}
+
+selectAllTableItems.addEventListener('change', function() {
+	if (this.checked) {
+		checkAllTableItems(true);
+	} else {
+		checkAllTableItems(false);
+	}
+});
+
+tableActionsClose.addEventListener('click', function() {
+	event.preventDefault();
+	checkAllTableItems(false);
+	selectAllTableItems.checked = false;
+});

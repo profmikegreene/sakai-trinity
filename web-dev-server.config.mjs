32680390b7453df0559cb09e7f1abd5895c88bf7
Michael Greene <profmikegreene@gmail.com>
2021-05-06 22:00:52 -0400

    attempt to fix style urls (#25)

---
+++ web-dev-server.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  open: '/app/',
+  nodeResolve: true,
+  watch: true,
+  // appIndex: '/app/',
+  // in a monorepo you need to set set the root dir to resolve modules
+  // rootDir: '../../',
+};

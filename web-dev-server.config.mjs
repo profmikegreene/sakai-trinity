827bb13be91dce73ec8044ceaf7df46d91929a75
Michael Greene <profmikegreene@gmail.com>
2021-05-06 22:37:27 -0400

    update webdevserver and fix css urls

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,8 +1,8 @@
 export default {
-  open: '/app/',
+  open: true,
   nodeResolve: true,
   watch: true,
-  // appIndex: '/app/',
+  appIndex: '/app/index.html',
   // in a monorepo you need to set set the root dir to resolve modules
-  // rootDir: '../../',
+  rootDir: '',
 };

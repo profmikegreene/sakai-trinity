d10cf5a93c29f918c8d791a40a0cba1c086e1358
Michael Greene <profmikegreene@gmail.com>
2021-08-03 11:42:01 -0400

    update sidebar to reflect some discussion in guerilla UX test

--- sakai-trinity/components/sakai-widget-user.js
+++ sakai-trinity/components/sakai-widget-user.js
@@ -17,39 +17,45 @@ export class SakaiWidgetUser extends LitElement {
   _onClick() {}
   render() {
     return html`
-    <link href="/sakai-trinity/components/sakai-widget-user.css" rel="stylesheet" type="text/css"> 
-    <div class="sakai-profile has-avatar">
+      <link
+        href="/sakai-trinity/components/sakai-widget-user.css"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <div class="sakai-profile has-avatar">
         <img src="/sakai-trinity/images/profile02.jpg" title="My Account" />
-        <div class="sakai-profile__changePic sr-only">Change Profile Picture</div>
+        <div class="sakai-profile__changePic sr-only">
+          Change Profile Picture
+        </div>
         <div class="sakai-profile__fullname">Sakai Instructor</div>
         <div class="sakai-profile__displayid">instructor</div>
-    </div>
-    <ul class="userNav" role="menu">
+      </div>
+      <ul class="userNav" role="menu">
         <li class="sakai-userNav__submenuitem">
-            <a href="#" class="sakai-headerItem">Account Info</a>
+          <a href="#" class="sakai-headerItem">Account Info</a>
         </li>
         <li class="sakai-userNav__submenuitem">
-            <a href="#" class="sakai-headerItem">Profile</a>
+          <a href="#" class="sakai-headerItem">Profile</a>
         </li>
         <li class="sakai-userNav__submenuitem">
-            <a href="#" class="sakai-headerItem">My Connections</a>
+          <a href="#" class="sakai-headerItem">My Connections</a>
         </li>
         <li class="sakai-userNav__submenuitem">
-            <a href="#" class="sakai-headerItem">Calendar</a>
+          <a href="#" class="sakai-headerItem">Calendar</a>
         </li>
         <li class="sakai-userNav__submenuitem">
-            <a href="#" class="sakai-headerItem">Preferences</a>
+          <a href="#" class="sakai-headerItem">Preferences</a>
         </li>
         <li class="sakai-userNav__submenuitem">
-            <a href="#" class="sakai-headerItem">Tutorial</a>
+          <a href="#" class="sakai-headerItem">Tutorial</a>
         </li>
         <li class="sakai-userNav__submenuitem">
-            <a href="#" class="sakai-headerItem">Themes</a>
+          <a href="#" class="sakai-headerItem">Themes</a>
         </li>
         <li class="sakai-userNav__submenuitem">
-            <a href="#" class="sakai-headerItem">Log Out</a>
+          <a href="#" class="sakai-headerItem">Log Out</a>
         </li>
-    </ul>
+      </ul>
     `;
   }
 }
